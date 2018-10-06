6b6ed2bee8bea345b9dc9c867423bfee45d21b15
Kackerlacka <Scoutamis123@gmail.com>
2018-10-06 16:04:21 -0700

    Further progressed Dimensionless Numbers Calculator

--- app/src/main/java/com/kackerlacka/mechie/ToolsDimensionlessNumbersCalc.java
+++ app/src/main/java/com/kackerlacka/mechie/ToolsDimensionlessNumbersCalc.java
@@ -4,24 +4,31 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.TextureView;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import katex.hourglass.in.mathlib.MathView;
 
 public class ToolsDimensionlessNumbersCalc extends AppCompatActivity {
 
     private Toolbar toolbar;
     private Spinner spinner1;
-    private Spinner spinner2;
+    private EditText input1, input2, input3, input4;
+    private TextView resultCalc;
     List<String> data1 = new ArrayList<>();
-    List<String> data2 = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,37 +48,44 @@ public class ToolsDimensionlessNumbersCalc extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Dimensionless Numbers Calculator");
 
-        addListenerOnButton();
+        initializeActivity();
+
+    }
+
+    public void calculateButton(View view) {
+        double a = Double.parseDouble(input1.getText().toString());
+        double b = Double.parseDouble(input2.getText().toString());
+        double c = Double.parseDouble(input3.getText().toString());
+        double d = Double.parseDouble(input4.getText().toString());
+        double result = ((double) a*b*c)/d;
+
+        String resultString = Double.toString(result);
+        NumberFormat formatter = new DecimalFormat("#0.00");
+        String resultStringFixed = formatter.format(resultString);
+        resultCalc = (TextView) findViewById(R.id.answer);
+        resultCalc.setText(resultStringFixed);
 
     }
 
     // get the selected dropdown list value
-    public void addListenerOnButton() {
+    public void initializeActivity() {
 
         spinner1 = (Spinner) findViewById(R.id.spinner1);
-        spinner2 = (Spinner) findViewById(R.id.sizeRange_value);
+        input1 = (EditText) findViewById(R.id.input_field1);
+        input2 = (EditText) findViewById(R.id.input_field2);
+        input3 = (EditText) findViewById(R.id.input_field3);
+        input4 = (EditText) findViewById(R.id.input_field4);
+        MathView dimensionlessFormula = (MathView) findViewById(R.id.formula);
 
         final ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_dropdown_item, data1);
 
-        final ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_dropdown_item, data2);
-
         spinner1.setAdapter(adapter1);
-        spinner2.setAdapter(adapter2);
 
-        data1.add("1");
-        data1.add("2");
-        data1.add("4");
-        data1.add("5");
-        data1.add("5.2");
-        data1.add("7");
-        data1.add("8");
-        data1.add("8.2");
-
+        data1.add("Reynold's Number (Re)");
+        data1.add("Mach Number (Ma)");
 
         adapter1.notifyDataSetChanged();
-        adapter2.notifyDataSetChanged();
 
         spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener()  {
 
@@ -83,106 +97,10 @@ public class ToolsDimensionlessNumbersCalc extends AppCompatActivity {
 
                 switch(spinner1.getSelectedItemPosition()) {
                     case 0:
-                        spinner2.setSelection(0);
-                        data2.clear();
-                        data2.add("¼ - 1½");
-                        adapter2.notifyDataSetChanged();
-                        proofStrength.setText("33");
-                        tensileStrength.setText("60");
-                        yieldStrength.setText("36");
+                        dimensionlessFormula.setDisplayText("<center>$$\\frac{\\rho v D}{\\mu}$$</center>");
                         break;
                     case 1:
-                        spinner2.setSelection(0);
-                        data2.clear();
-                        data2.add("¼ - ¾");
-                        data2.add("⅞ - 1½");
-                        adapter2.notifyDataSetChanged();
-                        proofStrength.setText("55");
-                        tensileStrength.setText("74");
-                        yieldStrength.setText("57");
-                        spinner2.setOnItemSelectedListener(new CustomOnItemSelectedListener() {
-                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                                if(spinner2.getSelectedItemPosition() == 0) {
-                                    proofStrength.setText("55");
-                                    tensileStrength.setText("74");
-                                    yieldStrength.setText("57");
-                                }
-                                else if(spinner2.getSelectedItemPosition() == 1){
-                                    proofStrength.setText("33");
-                                    tensileStrength.setText("60");
-                                    yieldStrength.setText("36");
-                                }
-                            }
-                        });
-                        break;
-                    case 2:
-                        spinner2.setSelection(0);
-                        data2.clear();
-                        data2.add("¼ - 1½");
-                        adapter2.notifyDataSetChanged();
-                        proofStrength.setText("65");
-                        tensileStrength.setText("115");
-                        yieldStrength.setText("100");
-                        break;
-                    case 3:
-                        spinner2.setSelection(0);
-                        data2.clear();
-                        data2.add("¼ - 1");
-                        data2.add("1⅛ - 1½");
-                        adapter2.notifyDataSetChanged();
-                        proofStrength.setText("85");
-                        tensileStrength.setText("120");
-                        yieldStrength.setText("92");
-                        spinner2.setOnItemSelectedListener(new CustomOnItemSelectedListener() {
-                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                                if(spinner2.getSelectedItemPosition() == 0) {
-                                    proofStrength.setText("85");
-                                    tensileStrength.setText("120");
-                                    yieldStrength.setText("92");
-                                }
-                                else if(spinner2.getSelectedItemPosition() == 1){
-                                    proofStrength.setText("74");
-                                    tensileStrength.setText("105");
-                                    yieldStrength.setText("81");
-                                }
-                            }
-                        });
-                        break;
-                    case 4:
-                        spinner2.setSelection(0);
-                        data2.clear();
-                        data2.add("¼ - 1");
-                        adapter2.notifyDataSetChanged();
-                        proofStrength.setText("85");
-                        tensileStrength.setText("120");
-                        yieldStrength.setText("92");
-                        break;
-                    case 5:
-                        spinner2.setSelection(0);
-                        data2.clear();
-                        data2.add("¼ - 1½");
-                        adapter2.notifyDataSetChanged();
-                        proofStrength.setText("105");
-                        tensileStrength.setText("133");
-                        yieldStrength.setText("115");
-                        break;
-                    case 6:
-                        spinner2.setSelection(0);
-                        data2.clear();
-                        data2.add("¼ - 1½");
-                        adapter2.notifyDataSetChanged();
-                        proofStrength.setText("120");
-                        tensileStrength.setText("150");
-                        yieldStrength.setText("130");
-                        break;
-                    case 7:
-                        spinner2.setSelection(0);
-                        data2.clear();
-                        data2.add("¼ - 1");
-                        adapter2.notifyDataSetChanged();
-                        proofStrength.setText("120");
-                        tensileStrength.setText("150");
-                        yieldStrength.setText("130");
+                        dimensionlessFormula.setDisplayText("<center>$$\\frac{\\sigma v D}{\\mu}$$</center>");
                         break;
 
                 }
